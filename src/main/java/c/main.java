448acece5bdaffb01542a62552a0da90b94189f8
zhuang2366 <2419982366@qq.com>
2020-09-04 15:48:02 +0800

    版本2

--- src/main/java/c/main.java
+++ src/main/java/c/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("hello Git");
 		System.out.println("hello Git  版本1" );
+		System.out.println("hello Git  版本2" );
 
 	}
 
